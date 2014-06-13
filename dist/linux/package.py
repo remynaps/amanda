771cc995fa9fb13ca6cd200911e2768632d896dd
Rubykuby <rubykuby@gmail.com>
2014-06-13 12:26:13 +0200

    Some improvements to package.py

--- dist/linux/package.py
+++ dist/linux/package.py
@@ -5,36 +5,39 @@ import shutil
 import sys
 
 
-package_name = "amanda_3.0-1"
-if len(sys.argv) > 1:
-    package_name = sys.argv[1]
+if len(sys.argv) > 1: package_name = sys.argv
+else:                 package_name = "amanda_3.0-1"
 
 def main():
     # Define some directory constants.
     LINUX_DIR   = os.path.dirname(os.path.abspath(__file__))
     PACKAGE_DIR = os.path.join(LINUX_DIR, package_name)
-    BIN_DIR     = os.path.abspath(os.path.join(LINUX_DIR, "../../bin"))
+    BIN_DIR     = os.path.abspath(os.path.join(LINUX_DIR, "..", "..", "bin"))
 
     # Clean the package tree if it exists.
     try:    shutil.rmtree(os.path.join(PACKAGE_DIR, "usr"))
     except: pass
 
     # Make the package tree.
-    os.makedirs(os.path.join(PACKAGE_DIR, "usr/local/bin"))
-    os.makedirs(os.path.join(PACKAGE_DIR, "usr/local/lib"))
+    os.makedirs(os.path.join(PACKAGE_DIR, "usr", "local", "bin"))
+    os.makedirs(os.path.join(PACKAGE_DIR, "usr", "local", "lib"))
 
     # Copy the files to the correct locations.
-    shutil.copyfile(os.path.join(BIN_DIR, "amanda"),
-                    os.path.join(PACKAGE_DIR, "usr/local/bin/amanda"))
-    shutil.copyfile(os.path.join(BIN_DIR, "libamanda.so"),
-                    os.path.join(PACKAGE_DIR, "usr/local/lib/libamanda.so"))
-    shutil.copyfile(os.path.join(BIN_DIR, "amanda.ini"),
-                    os.path.join(PACKAGE_DIR, "usr/local/lib/amanda.ini"))
+    shutil.copyfile(
+        os.path.join(BIN_DIR, "amanda"),
+        os.path.join(PACKAGE_DIR, "usr", "local", "bin", "amanda"))
+    shutil.copyfile(
+        os.path.join(BIN_DIR, "libamanda.so"),
+        os.path.join(PACKAGE_DIR, "usr", "local", "lib", "libamanda.so"))
+    shutil.copyfile(
+        os.path.join(BIN_DIR, "amanda.ini"),
+        os.path.join(PACKAGE_DIR, "usr", "local", "lib", "amanda.ini"))
 
     # Sort out permissions
     os.system("chown -R root:root {}".format(os.path.join(PACKAGE_DIR, "usr")))
-    os.system("chmod a+x {}".format(os.path.join(PACKAGE_DIR,
-                                                 "usr/local/bin/amanda")))
+    os.system("chmod a+x {}".format(os.path.join(
+        PACKAGE_DIR,
+        "usr", "local", "bin", "amanda")))
 
     # Build the .deb package
     os.system("dpkg -b {}".format(PACKAGE_DIR))
